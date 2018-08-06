5894aa5fca6399c3025f724eae4cfd78e578d3a5
Alex Stephen <alexstephen@google.com>
2018-08-06 20:48:17 +0000

    Better Class Names

--- libraries/google/storage/property/objectaccesscontrol_project_team.rb
+++ libraries/google/storage/property/objectaccesscontrol_project_team.rb
@@ -29,7 +29,7 @@ module Google
   module Storage
     module Data
       # A class to manage data for ProjectTeam for object_access_control.
-      class ObjeAcceContProjTeam
+      class ObjectAccessControlProjectTeam
         include Comparable
 
         attr_reader :project_number
@@ -50,7 +50,7 @@ module Google
         end
 
         def ==(other)
-          return false unless other.is_a? ObjeAcceContProjTeam
+          return false unless other.is_a? ObjectAccessControlProjectTeam
           compare_fields(other).each do |compare|
             next if compare[:self].nil? || compare[:other].nil?
             return false if compare[:self] != compare[:other]
@@ -59,7 +59,7 @@ module Google
         end
 
         def <=>(other)
-          return false unless other.is_a? ObjeAcceContProjTeam
+          return false unless other.is_a? ObjectAccessControlProjectTeam
           compare_fields(other).each do |compare|
             next if compare[:self].nil? || compare[:other].nil?
             result = compare[:self] <=> compare[:other]
@@ -82,18 +82,18 @@ module Google
         end
       end
 
-      # Manages a ObjeAcceContProjTeam nested object
+      # Manages a ObjectAccessControlProjectTeam nested object
       # Data is coming from the GCP API
-      class ObjeAcceContProjTeamApi < ObjeAcceContProjTeam
+      class ObjectAccessControlProjectTeamApi < ObjectAccessControlProjectTeam
         def initialize(args)
           @project_number = Google::Storage::Property::String.api_parse(args['projectNumber'])
           @team = Google::Storage::Property::Enum.api_parse(args['team'])
         end
       end
 
-      # Manages a ObjeAcceContProjTeam nested object
+      # Manages a ObjectAccessControlProjectTeam nested object
       # Data is coming from the Chef catalog
-      class ObjeAcceContProjTeamCatalog < ObjeAcceContProjTeam
+      class ObjectAccessControlProjectTeamCatalog < ObjectAccessControlProjectTeam
         def initialize(args)
           @project_number = Google::Storage::Property::String.catalog_parse(args[:project_number])
           @team = Google::Storage::Property::Enum.catalog_parse(args[:team])
@@ -103,23 +103,23 @@ module Google
 
     module Property
       # A class to manage input to ProjectTeam for object_access_control.
-      class ObjeAcceContProjTeam
+      class ObjectAccessControlProjectTeam
         def self.coerce
-          ->(x) { ::Google::Storage::Property::ObjeAcceContProjTeam.catalog_parse(x) }
+          ->(x) { ::Google::Storage::Property::ObjectAccessControlProjectTeam.catalog_parse(x) }
         end
 
         # Used for parsing Chef catalog
         def self.catalog_parse(value)
           return if value.nil?
-          return value if value.is_a? Data::ObjeAcceContProjTeam
-          Data::ObjeAcceContProjTeamCatalog.new(value)
+          return value if value.is_a? Data::ObjectAccessControlProjectTeam
+          Data::ObjectAccessControlProjectTeamCatalog.new(value)
         end
 
         # Used for parsing GCP API responses
         def self.api_parse(value)
           return if value.nil?
-          return value if value.is_a? Data::ObjeAcceContProjTeam
-          Data::ObjeAcceContProjTeamApi.new(value)
+          return value if value.is_a? Data::ObjectAccessControlProjectTeam
+          Data::ObjectAccessControlProjectTeamApi.new(value)
         end
       end
     end
